7aef2a4a2857b58aa74847ffb63f215ae545bc21
Mauro Daisuke <mauro.daisuke@hotmail.com>
2023-07-18 15:45:09 -0400

    Adicionado estilização no Components Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import "./styles.css";
+
 import Logo from "../../objects/Logo";
 import Sobre from "../../objects/Sobre";
 import Hamburguer from "../../objects/Hamburguer";
 
 const Header = () => {
   return (
-    <>
+    <header className="header">
       <Logo />
       <Sobre />
       <Hamburguer />
-    </>
+    </header>
   );
 };
 
--- src/objects/Hamburguer/index.js
+++ src/objects/Hamburguer/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Menu from "../../assets/icon-nav.png";
 
 const Hamburguer = () => {
-  return <img src={Menu} alt="menu-hamburguer" />;
+  return <img src={Menu} className="menu" alt="menu-hamburguer" />;
 };
 
 export default Hamburguer;
